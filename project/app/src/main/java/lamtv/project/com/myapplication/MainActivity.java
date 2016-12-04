01dd5bba81f39f2f8319d744f03c0f7fe2227744
XuanLV <xuanlv@steadfast-inc.con>
2016-12-04 20:58:53 +0700

    add firebase to project

--- project/app/src/main/java/lamtv/project/com/myapplication/MainActivity.java
+++ project/app/src/main/java/lamtv/project/com/myapplication/MainActivity.java
@@ -4,15 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
 public class MainActivity extends FragmentActivity {
-
-
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Query recentPostsQuery = mDatabase.child("travles")
+                .limitToFirst(100);
     }
 
 }
