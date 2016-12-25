dc252108352ad6f641821790dbb77f42265694b0
XuanLV <xuanlv@steadfast-inc.con>
2016-12-25 23:18:45 +0700

    default location

--- project/app/src/main/java/lamtv/project/com/myapplication/MapsActivity.java
+++ project/app/src/main/java/lamtv/project/com/myapplication/MapsActivity.java
@@ -50,7 +50,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         travles = (Travles) getIntent().getSerializableExtra("TRAVLES");
         arr = new ArrayList<>();
-        endLocation = new LatLng(Double.valueOf(travles.getCoordinate_1().split(",")[0]),Double.valueOf(travles.getCoordinate_1().split(",")[1].replace(" ","")));
+        endLocation = new LatLng(Double.valueOf(travles.getCoordinate_1().split(",")[0]), Double.valueOf(travles.getCoordinate_1().split(",")[1].replace(" ", "")));
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         mRecyclerView = (RecyclerView) findViewById(R.id.rcvMap);
         mRecyclerView.setHasFixedSize(true);
@@ -73,14 +73,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-         location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng mylocation = new LatLng(location.getLatitude(), location.getLongitude());
+        LatLng mylocation;
+        if (location == null) {
+            mylocation = new LatLng(21.0489522, 105.7832983);
+        } else {
+            mylocation = new LatLng(location.getLatitude(), location.getLongitude());
+        }
         mMap.addMarker(new MarkerOptions().position(mylocation).title(""));
         mMap.addMarker(new MarkerOptions().position(endLocation).title(travles.getName()));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
@@ -91,11 +96,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ReadTask readTask = new ReadTask();
         readTask.execute("");
     }
+
     private class ReadTask extends AsyncTask<String, Void, Document> {
         @Override
         protected org.w3c.dom.Document doInBackground(String... url) {
 
-            org.w3c.dom.Document doc = md.getDocument(new LatLng(location.getLatitude(),location.getLongitude()),endLocation , lamtv.project.com.myapplication.fragment.Document.MODE_DRIVING);
+            LatLng mylocation;
+            org.w3c.dom.Document doc;
+            if (location == null) {
+                mylocation = new LatLng(21.0489522, 105.7832983);
+                doc = md.getDocument(mylocation, endLocation, lamtv.project.com.myapplication.fragment.Document.MODE_DRIVING);
+
+            } else {
+                doc = md.getDocument(new LatLng(location.getLatitude(), location.getLongitude()), endLocation, lamtv.project.com.myapplication.fragment.Document.MODE_DRIVING);
+            }
 
             return doc;
         }
@@ -107,7 +121,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mAdapter.notifyDataSetChanged();
             ArrayList<LatLng> directionPoint = md.getDirection(result);
             PolylineOptions rectLine = new PolylineOptions().width(10).color(Color.BLUE); // Màu và độ rộng[/FONT]
-            for(int i = 0 ; i < directionPoint.size() ; i++) {
+            for (int i = 0; i < directionPoint.size(); i++) {
                 rectLine.add(directionPoint.get(i));
             }
             mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
